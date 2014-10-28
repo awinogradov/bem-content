aadfb0dcf0abdc85263a609390971917f4c65f5a
Anton Winogradov <winogradovaa@gmail.com>
2014-10-28 15:22:26 +0300

    build promo page

--- .bem/make.js
+++ .bem/make.js
@@ -72,11 +72,10 @@ MAKE.decl('BundleNode', {
         return [
             'libs/bem-core/common.blocks',
             'libs/bem-components/common.blocks',
-            'libs/bem-components/design/common.blocks',
             'libs/bem-components/desktop.blocks',
-            'libs/bem-components/design/desktop.blocks',
             'common.blocks',
-            'design/common.blocks',
+            'promo.blocks',
+            'design/promo.blocks',
             'test.blocks'
         ];
     },
